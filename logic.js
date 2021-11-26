35e78093295d0519be8c5f0a098baa11f7c3305f
Angie Sanchez <angiesan.0607@gmail.com>
2021-11-25 21:08:21 -0500

    READY!!!

--- logic.js
+++ logic.js
@@ -1,8 +1,9 @@
-const btn = document.querySelector("#enviar");
-const nombre = document.querySelector("#nombre");
-const email = document.querySelector("#email");
-const mensaje = document.querySelector("#mensaje");
-const form = document.querySelector("#formulario");
+
+const btn = document.querySelector('#enviar');
+const nombre = document.querySelector('#nombre')
+const email = document.querySelector('#email')
+const mensaje = document.querySelector('#mensaje')
+const form = document.querySelector('#formulario')
 // MENU IN RESPOSIVE
 const menuIcon = document.querySelector(".navbar");
 const menu = document.querySelector(".menu");
@@ -32,10 +33,14 @@ function EventListener() {
 	email.addEventListener('blur', validarCampo)
 	mensaje.addEventListener('blur', validarCampo)
 }
+
+
 function iniciarApp() {
 	btn.disabled = true;
 	btn.classList.add('not-allowed', 'opacity-50')
 }
+
+
 function validarCampo(e) {
 	if (e.target.value.length > 0) {
 		//Elimina los errores
@@ -44,6 +49,7 @@ function validarCampo(e) {
 			error.remove();
 		}
 
+		
 		e.target.classList.remove('border', 'border-danger')
 		e.target.classList.add('border', 'border-success')
 
@@ -56,6 +62,7 @@ function validarCampo(e) {
 
 	if (e.target.type === 'email') {
 		if (er.test(e.target.value)) {
+			
 			e.target.classList.remove('border', 'border-danger')
 			e.target.classList.add('border', 'border-success')
 			const error = document.querySelector('p.error')
@@ -64,7 +71,7 @@ function validarCampo(e) {
 			}
 
 		} else {
-			console.log('Email No Valido')
+			
 			mostrarError('El email no es valido')
 			e.target.classList.remove('border', 'border-success')
 			e.target.classList.add('border', 'border-danger')
@@ -73,36 +80,28 @@ function validarCampo(e) {
 	}
 
 	if (er.test(email.value) && email.value != "" && mensaje.value != "") {
+		
 		btn.disabled = false;
 		btn.classList.remove('not-allowed', 'opacity-50')
 
 	} else {
 		btn.disabled = true;
 		btn.classList.add('not-allowed', 'opacity-50')
-	
+		
 	}
 
 }
 
-// TYPED
-const typed = new Typed(".typed", {
-  strings: [
-    
-    '<i class="text-typed">Full Stack Developer</i>',
-	'<i class="text-typed">Systems Technician</i>',
-	'<i class="text-typed">Front-end developer</i>',
-    '<i class="text-typed">Back-end developer</i>',
-  ],
-  typeSpeed: 75, 
-  startDelay: 300, 
-  backSpeed: 75,
-  smartBackspace: true, 
-  shuffle: false, 
-  backDelay: 1500, 
-  loop: true, 
-  loopCount: false, 
-  showCursor: true, 
-  cursorChar: '<i class="cursosChar">|</i>', 
-  contentType: "html", 
-});
+function mostrarError(mensaje) {
+
+	const mensajeError = document.createElement('p');
+	mensajeError.textContent = mensaje;
+	mensajeError.classList.add('text-center', 'border', 'border-danger', 'p-3', 'mt-2', 'text-danger', 'error')
+
+	const errores = document.querySelectorAll('.error')
+	if (errores.length === 0) {
+		form.appendChild(mensajeError)
+	}
+
+}
 
